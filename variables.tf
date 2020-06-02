3a3408161996cba1fb4e9b1f23b2a6c4914f4d72
Gabor Balogh <gabor.balogh1@gmail.com>
2020-06-02 20:12:41 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,3 +18,17 @@ variable "ami" {
   # Bitnami NGINX AMI
   default = "ami-021acbdb89706aa89"
 }
+
+variable "region"{
+	default = "eu-west-2"
+}
+
+variable "access_key"{
+	type = string
+    default = "AKIAQ7AUKM66C3ERNJIZ"
+}
+
+variable "secret_key"{
+	type = string
+    default = "9/Q1TOttExu06LGGfmw4sJTFPEWUo4mL9Fs7kt4I"
+}
