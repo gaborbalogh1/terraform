5cd8a7d5f9685b02ad001d621a34836c4d160f7e
Gabor Balogh <gabor.balogh1@gmail.com>
2020-06-04 18:41:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,12 +13,11 @@ provider "aws" {
 
 resource "aws_eip_association" "eip_assoc" {
   instance_id   = "aws_instance.default[count.index]"
-  allocation_id = "aws_eip.default.allocation_id"
+  allocation_id = "${aws_eip.default.id}"
 }
 
 resource "aws_eip" "default" {
   vpc = true
-  allocation_id = "eipalloc-07ee242e2adb649f2"
 }
 
 # Create EC2 instance
