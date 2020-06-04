fa4112ad24ae396972cce811d5371e2f5262b7d7
Gabor Balogh <gabor.balogh1@gmail.com>
2020-06-04 21:04:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,13 +15,13 @@ provider "aws" {
 resource "aws_instance" "default" {
   ami                    = var.ami
   count                  = var.instance_count
-  key_name	         = var.key_name
+  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.default.id]
   source_dest_check      = false
-  instance_type          = var.instance_type	
-	
+  instance_type          = var.instance_type
+
   tags = {
-    Name = "terraform-default"
+    Name = "Terraform-EC2_V1"
   }
 }
 
