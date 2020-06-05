487511715dff1cc66a6ac88a6e2e570ad05d04a4
Gabor Balogh <gabor.balogh1@gmail.com>
2020-06-05 10:10:35 +0100

    added 4 more instance and another sg
    example instance is back now 4 (3simple ami and 1 with .net core and pw )
    also will add a ssh key to all 4 servers once all build and confirmed.

--- main.tf
+++ main.tf
@@ -25,6 +25,67 @@ resource "aws_instance" "default" {
   }
 }
 
+resource "aws_instance" "example" {
+
+	ami = "ami-01a6e31ac994bbc09"
+	instance_type = "t2.micro"
+	vpc_security_group_ids = [aws_security_group.Terraform_Examples.id]
+	  tags = {
+    Name = "terraform-Example1"
+  }
+	
+	provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+
+}
+
+resource "aws_instance" "example2" {
+
+	ami = "ami-01a6e31ac994bbc09"
+	instance_type = "t2.micro"
+	vpc_security_group_ids = [aws_security_group.Terraform_Examples.id]
+		  tags = {
+    Name = "terraform-Example2"
+  }
+	provisioner "local-exec" {
+    command = "echo ${aws_instance.example2.public_ip} > ip_address.txt"
+  }
+
+}
+
+
+resource "aws_instance" "example3" {
+
+	ami = "ami-01a6e31ac994bbc09"
+	instance_type = "t2.micro"
+        vpc_security_group_ids = [aws_security_group.Terraform_Examples.id]
+	tags = {
+    Name = "terraform-Example3"
+  }
+	
+	provisioner "local-exec" {
+    command = "echo ${aws_instance.example3.public_ip} > ip_address.txt"
+  }
+
+}
+
+resource "aws_instance" "PwsLinux" {
+
+	ami = "ami-06b6dafd50fc45e21"
+	instance_type = "t2.micro"
+	vpc_security_group_ids = [aws_security_group.Terraform_Examples.id]
+	
+        tags = {
+    Name = "terraform-PwsLinux"
+  }
+	
+	provisioner "local-exec" {
+    command = "echo ${aws_instance.PwsLinux.public_ip} > ip_address.txt"	
+  }
+
+}
+
 # Create Security Group for EC2
 resource "aws_security_group" "default" {
   name = "terraform-default-sg"
@@ -42,5 +103,32 @@ resource "aws_security_group" "default" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+}
+
+# Create Security Group for EC2_v2
+resource "aws_security_group" "Terraform_Examples" {
+  name = "terraform-Terraform_Examples-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+	
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }	
 
 }
